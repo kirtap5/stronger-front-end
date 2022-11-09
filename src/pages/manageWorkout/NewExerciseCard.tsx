186de12767fb3900cd55a84d4aa17a2c5bec99a3
Patrik Gerdelius <patrik.gerdelius@hotmail.com>
2022-11-09 17:24:44 +0100

    refactored with grid layout

--- src/pages/manageWorkout/NewExerciseCard.tsx
+++ src/pages/manageWorkout/NewExerciseCard.tsx
@@ -5,15 +5,11 @@ import { BiPlus } from "react-icons/bi";
 export const NewExerciseCard = () => {
   return (
     <button style={styles.root}>
-      <div style={styles.topContainer}>
-        <small style={styles.highlightText}>Select</small>
-        <small style={styles.date}>3:17 PM</small>
-      </div>
-      <div style={styles.bottomContainer}>
-        <b style={styles.caption}>Select a new exersice...</b>
+      <small style={styles.highlightText}>Select</small>
+      <small style={styles.date}>3:17 PM</small>
+      <b style={styles.caption}>Select a new exersice...</b>
 
-        <BiPlus size={35} />
-      </div>
+      <BiPlus size={35} style={styles.icon} />
     </button>
   );
 };
@@ -23,33 +19,30 @@ const styles: StyleType = {
     backgroundColor: colors.lightGrey,
     margin: " 200px auto",
     width: "350px",
-    height: "200px",
+    height: "80px",
     border: "none",
     borderRadius: "10px",
-    padding: "15px 15px",
-    display: "flex",
-    alignItems: "start",
-    flexWrap: "wrap",
-    alignContent: "space-between",
-  },
-  topContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "100%",
-  },
-  bottomContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "100%",
+    padding: "10px 15px",
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)",
+    gridTemplateRows: "repeat(3, 1fr)",
+    alignItems: "center",
+    rowGap: "5px",
   },
   highlightText: {
     color: colors.secondary,
+    justifySelf: "baseline",
   },
   caption: {
     marginTop: "10px",
     color: colors.black,
+    justifySelf: "baseline",
   },
   date: {
     color: colors.grey,
+    justifySelf: "flex-end",
+  },
+  icon: {
+    justifySelf: "flex-end",
   },
 };
